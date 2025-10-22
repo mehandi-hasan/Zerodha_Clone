3077b62be3ac35f0efecd2042f1b27879c5ffe2d
Mehandi Hasan <145278479+mehandi-hasan@users.noreply.github.com>
2025-10-22 16:28:41 +0530

    Add files via upload

---
+++ frontend/src/landing_page/products/RightSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function RightSection({
+    imageURL,
+    productName,
+    productDescription,
+    tryDemo,
+    learnMore,
+    googlePlay,
+    appStore,
+}) {
+    return ( 
+       <div className='conatiner mt-5'>
+    <div className='row p-5'>
+         <div className='col-6 p-5 mt-5'>
+            <h1>{productName}</h1>
+            <p>{productDescription}</p>
+             <div>
+                 
+                 <a href={learnMore}  style={{textDecoration:"none"}}>Learn More  <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+             </div>
+            
+        </div>
+        <div className='col-6 '>
+            <img src={imageURL} />
+        </div>
+       
+    </div>
+  </div>
+     );
+}
+
+export default RightSection;
---
+++ frontend/src/landing_page/products/Universe.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+function Universe() {
+    return (  
+        <div className='conatiner mt-5 text-center' >
+    <div className='row p-5'>
+        <h1 >The Zerodha Universe</h1>
+        <p >Extend your trading and investment experience even further with our partner platforms</p>
+         
+        <div className='col-4 p-3 mt-5'>
+            <img src="media/smallcaseLogo.png" />
+            <p className='text-muted text-small'>Our asset management venture
+that is creating simple and transparent index
+funds to help you save for your goals.</p>
+        </div>
+         <div className='col-4 p-3 mt-5'>
+            <img src="media/smallcaseLogo.png" />
+            <p className='text-muted text-small'>Our asset management venture
+that is creating simple and transparent index
+funds to help you save for your goals.</p>
+        </div>
+         <div className='col-4 p-3 mt-5'>
+             <img src="media/smallcaseLogo.png" />
+            <p className='text-muted text-small'>Our asset management venture
+that is creating simple and transparent index
+funds to help you save for your goals.</p>
+        </div>
+       
+        <div className='col-4 p-3 mt-5'>
+            <img src="media/smallcaseLogo.png" />
+            <p className='text-muted text-small'>Our asset management venture
+that is creating simple and transparent index
+funds to help you save for your goals.</p>
+        </div>
+         <div className='col-4 p-3 mt-5'>
+            <img src="media/smallcaseLogo.png" />
+            <p className='text-muted text-small'>Our asset management venture
+that is creating simple and transparent index
+funds to help you save for your goals.</p>
+        </div>
+         <div className='col-4 p-3 mt-5'>
+             <img src="media/smallcaseLogo.png" />
+            <p className='text-muted text-small'>Our asset management venture
+that is creating simple and transparent index
+funds to help you save for your goals.</p>
+        </div>
+       <button className='p-2 btn btn-primary fs-5 mb-5 mt-5' style={{width:"20%", margin:"0 auto"}}>Sign up for free</button>
+    </div>
+  </div>
+    );
+}
+
+export default Universe;
